75f10b916161079d11f7f504fa79f303b8970679
guibarreira07-byte <gui.barreira07@gmail.com>
2026-05-04 15:35:48 -0300

    Update cleanup-orphan-public-ips.ps1
    # Connect to Azure
    Connect-AzAccount
    
    # Get all Public IPs
    $publicIps = Get-AzPublicIpAddress
    
    foreach ($ip in $publicIps) {
        if ($ip.IpConfiguration -eq $null -and $ip.Name -like "*bots*") {
            
            Write-Output "Removing orphan Public IP: $($ip.Name) in RG: $($ip.ResourceGroupName)"
            
            Remove-AzPublicIpAddress `
                -Name $ip.Name `
                -ResourceGroupName $ip.ResourceGroupName `
                -Force
        }
    }

--- cleanup-orphan-public-ips.ps1
+++ cleanup-orphan-public-ips.ps1
@@ -1,1 +1,17 @@
+# Connect to Azure
+Connect-AzAccount
 
+# Get all Public IPs
+$publicIps = Get-AzPublicIpAddress
+
+foreach ($ip in $publicIps) {
+    if ($ip.IpConfiguration -eq $null -and $ip.Name -like "*bots*") {
+        
+        Write-Output "Removing orphan Public IP: $($ip.Name) in RG: $($ip.ResourceGroupName)"
+        
+        Remove-AzPublicIpAddress `
+            -Name $ip.Name `
+            -ResourceGroupName $ip.ResourceGroupName `
+            -Force
+    }
+}
